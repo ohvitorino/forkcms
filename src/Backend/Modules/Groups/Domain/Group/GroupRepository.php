efadb706357bb8560d98a96dc8897e35c7dfca66
Bruno Vitorino <bruno@sumocoders.be>
2018-03-07 17:05:30 +0100

    Add method to create a group

--- src/Backend/Modules/Groups/Domain/Group/GroupRepository.php
+++ src/Backend/Modules/Groups/Domain/Group/GroupRepository.php
@@ -6,4 +6,9 @@ use Doctrine\ORM\EntityRepository;
 
 class GroupRepository extends EntityRepository
 {
+    public function add(Group $group): void
+    {
+        $this->getEntityManager()->persist($group);
+        $this->getEntityManager()->flush($group);
+    }
 }
